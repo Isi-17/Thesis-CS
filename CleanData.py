52e670c55c9ac5a2a724b8b79685c3d5d96ee70c
Isidro <isidrojova@gmail.com>
2024-02-12 13:48:58 +0100

    Modify comments in CleanData.py and rewrite the code to make it more readable

--- CleanData.py
+++ CleanData.py
@@ -12,41 +12,57 @@ with open(ficheroEntrada, 'r') as file:
 for line in lines:
     # Divido la línea en tres partes: ID, date y consumo
     ID, date, consumption = line.split()
-    # date == dia && nº media hora -> 19501 -> Dia 195. Hora 0
-    #                              -> 19648 -> Dia 196. Hora 23
 
-    # Calculo la hora a la que corresponde el consumo. La hora viene dada en medias horas desde 01 hasta 48 (en medias horas)
-    half_hour = int(date[3:5])
-    hour = (int(date[3:5]) - 1) // 2  # [0, 23]
-    # 01 -> Hora 0,  22 -> Hora 11,  48 -> Hora 23
+    # Dia: primeras 3 cifras de la fecha
     day = int(date[:3])
+
+    # Hora: últimas 2 cifras de la fecha. La hora viene dada en medias horas desde 01 hasta 48
+    half_hour = int(date[3:5])  # [1, 48]
+    hour = (half_hour - 1) // 2  # [0, 23]
+    
     # Si el ID no está en el diccionario, lo añado
     if ID not in data:
         data[ID] = {}
 
-    # Si el día no está en el diccionario, lo añado y le asigno una lista de 48 elementos con [fecha, consumo[0], consumo[1], ..., consumo[47]]
+    # Si el día no está en el diccionario, lo añado y le asigno una lista de 48 elementos: dia ++ 47 valores None correspindeintes a las medias horas. 
     if day not in data[ID]:
-        data[ID][day] = [day] + [None] * 47 # 1 - 48
+        data[ID][day] = [day] + [None] * 47 # [fecha, consumo[0], consumo[1], ..., consumo[47]]
 
-    # Cojo todos los datos, tanto para las medias horas impares como para las pares
     if 1 <= half_hour <= 48:
         data[ID][day][half_hour - 1] = float(consumption) * 1000  # kWH -> wH
-    # print(ID, day)
-    # print(data[ID][day])
         
 # Elimino las líneas con valores None
-data_cleaned = {ID: {day: consumption_list for day, consumption_list in dateANDconsumption.items() if None not in consumption_list} for ID, dateANDconsumption in data.items()}
+data_cleaned = {
+    ID: {
+        day: consumption_list
+        for day, consumption_list in dateANDconsumption.items() 
+        if None not in consumption_list
+    }
+    for ID, dateANDconsumption in data.items()
+}
 
 # Ahora sumo los valores de las primeras y segundas medias horas para obtener los 24 valores por hora
-data_hourly = {ID: {day: [day] + [sum(data[ID][day][i:i + 2]) for i in range(0, 48, 2)] for day in dateANDconsumption} for ID, dateANDconsumption in data_cleaned.items()}
+data_hourly = {
+    ID: {
+        day: [day] + [sum(data[ID][day][i:i + 2]) for i in range(0, 48, 2)]
+        for day in dateANDconsumption
+    }
+    for ID, dateANDconsumption in data_cleaned.items()
+} # [fecha, consumo[0], consumo[1], ..., consumo[23]]
 
 # Aplico los filtros adicionales:
 # - El consumo total del día debe ser mayor de 100 Wh
 # - El consumo máximo en una hora no debe superar los 15000 Wh
-filtered_data = {ID: {day: consumption_list for day, consumption_list in dateANDconsumption.items() if sum(consumption_list[1:]) > 100 and max(consumption_list[1:]) <= 15000} for ID, dateANDconsumption in data_hourly.items()}
+filtered_data = {
+    ID: {
+        day: consumption_list
+        for day, consumption_list in dateANDconsumption.items()
+        if sum(consumption_list[1:]) > 100 and max(consumption_list[1:]) <= 15000
+    }
+    for ID, dateANDconsumption in data_hourly.items()
+}
 
-
-# Ahora que tengo el diccionario, voy a crear el CSV
+# Creación del fichero CSV
 with open(ficheroSalida, 'w', newline='') as file:
     writer = csv.writer(file)
     # Escribo la cabecera
@@ -54,4 +70,4 @@ with open(ficheroSalida, 'w', newline='') as file:
     # Recorro el diccionario y escribo cada fila
     for ID, dateANDconsumption in data_hourly.items():
         for day, consumption_list in dateANDconsumption.items():
-            writer.writerow([ID, *consumption_list])  # Uso el operador * para desempaquetar la lista y escribir cada elemento como una columna
+            writer.writerow([ID, *consumption_list])
