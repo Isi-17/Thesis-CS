5dd0b9d11a7d4ba07abd343f1346dd397f9c4b2c
Isidro García <isidrojova@gmail.com>
2024-02-13 21:57:43 +0100

    Update CentroidCount.py

--- CentroidCount.py
+++ CentroidCount.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-# Cargar el CSV en un DataFrame
 df = pd.read_csv("FinalData+Cluster.csv")
 
 # -----------------------------
@@ -11,7 +10,7 @@ conteo_centroides_num = df['Centroide'].value_counts()
 print("Conteo de veces que se repite cada Centroide orden descendente:")
 print(conteo_centroides_num)
 
-# Crear un CSV con el conteo_centroides_ord
+# Guardo en un CSV con el conteo_centroides_ord
 conteo_centroides_num.to_csv('ConteoCentroidesDesc.csv')
 
 # Centroide
@@ -44,7 +43,7 @@ conteo_centroides_ord = df['Centroide'].value_counts().sort_index()
 print("Conteo de veces que se repite cada Centroide:")
 print(conteo_centroides_ord)
 
-# Crear un CSV con el conteo_centroides_ord
+# Guardo en un CSV con el conteo_centroides_ord
 conteo_centroides_ord.to_csv('ConteoCentroidesOrd.csv')
 
 # Centroide
@@ -78,4 +77,5 @@ agrupado_por_usuario_centroide = df.groupby(['ID', 'Centroide']).size().reset_in
 # Utilizar pivot_table para obtener la tabla deseada
 tabla_usuario_centroide = agrupado_por_usuario_centroide.pivot_table(index='ID', columns='Centroide', values='Conteo', fill_value=0)
 
+# Guardo en un CSV con la distribución de los centroides.
 tabla_usuario_centroide.to_csv('DistribuciónCentroides.csv')
