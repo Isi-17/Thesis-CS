75166cb55a3c57dbfc96e42d19b3d3f59db45431
Isidro <isidrojova@gmail.com>
2024-02-17 09:57:07 +0100

    Fixed distribution filter

--- FilterLowCentroids.py
+++ FilterLowCentroids.py
@@ -4,20 +4,15 @@ import pandas as pd
 df = pd.read_csv('DistribuciónCentroides.csv')
 
 # Especificar el umbral de número mínimo de clusters diferentes (sobre 20)
-umbral_clusters = 2
+umbral_clusters = 5
 
 # Filtrar IDs con menor número de clusters diferentes que el umbral
 filtered_rows = []
 for index, row in df.iterrows():
     clusters = row[1:]  # [f(0), ..., f(20)]
-    clusters_cleaned = [c for c in clusters if c != 0.0]
+    clusters_cleaned = [c for c in clusters if c != 0] # eliminamos los clusters no usados
 
-    unique_clusters = []
-    for c in clusters_cleaned:
-        if c not in unique_clusters:
-            unique_clusters.append(c)
-
-    clusters_diferentes = len(unique_clusters)
+    clusters_diferentes = len(clusters_cleaned)
     
     if clusters_diferentes <= umbral_clusters:
         filtered_rows.append(row)
@@ -28,4 +23,4 @@ result_df = pd.DataFrame(filtered_rows)
 
 
 # Genero un CSV con los resultados filtrados
-result_df.to_csv('filter2LowCentroids.csv', index = False)
+result_df.to_csv('filter5LowCentroids.csv', index = False)
