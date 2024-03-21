ceeddce1346c714af3a0448358836885800cfc71
Isi-17 <isidrojova@uma.es>
2024-03-21 10:36:47 +0100

    Rewritten code

--- ChooseCentroid.py
+++ ChooseCentroid.py
@@ -2,31 +2,29 @@ import pandas as pd
 import numpy as np
 
 # Read the dataset with hourly energy consumptions
-df_data_consumptions = pd.read_csv('FinalData.csv', delimiter=',')
+final_data_df = pd.read_csv('FinalData.csv', delimiter=',')
 
 # Read the dataset with centroids
-df_centroids = pd.read_csv('ireland_centroids.csv', delimiter=';')
+centroids_data_df = pd.read_csv('ireland_centroids.csv', delimiter=';')
 
 # Add a column for the associated centroid to each consumption row
-df_data_consumptions['Centroid'] = np.nan
+final_data_centroid_df = final_data_df[['Day']].copy()
+final_data_centroid_df['Centroid'] = np.nan
 
 # Change decimal format of consumption columns
-df_centroids.iloc[:, 1:] = df_centroids.iloc[:, 1:].replace(',', '.', regex=True).astype(float)
+centroids_data_df = centroids_data_df.replace(',', '.', regex=True).astype(float)
 
 # Iterate through each row in the consumption dataset
-for index, row in df_data_consumptions.iterrows():
-    # Extract hourly consumption values from the row
-    hour_consumption = row[2:-1]  # [hour_1, hour_2, ..., hour_24]
+for i in range(len(final_data_df)):
+    # Extract hourly consumption values from the row. row[1:-1] excludes the first and last columns: 'Day' and 'Centroid'
+    consumption_array = np.array(final_data_df.iloc[i, 1:-1], dtype=float) # [hour_1, hour_2, ..., hour_24]
 
-    consumption_array = hour_consumption.values.astype(float)
-    
     # Calculate the Euclidean distance to each centroid
-    distances = np.linalg.norm(df_centroids.iloc[:, 1:].values - consumption_array, axis=1)
-    # 1: Skip the first column with row names: '0' '1' ... '20'
+    distances = np.linalg.norm(centroids_data_df.values - consumption_array, axis=1)
     # distances = [distance_centroid_1, distance_centroid_2, ..., distance_centroid_21]
- 
-    # Assign the centroid with the lowest distance
-    df_data_consumptions.at[index, 'Centroid'] = chr(65 + int(np.argmin(distances)))
+
+    # Assign the centroid with the lowest distance as letters A, B, C, ..., U
+    final_data_centroid_df.iloc[i, -1] = chr(65 + int(np.argmin(distances)))
 
 # Save the updated dataset with assigned centroids
-df_data_consumptions.to_csv('FinalData+Cluster.csv', index=False)
+final_data_centroid_df.to_csv('final_data_cluster.csv', index=True)
